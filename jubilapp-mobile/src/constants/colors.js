77639edaa8b1216f265173e95478c08f7f965327
pauleta_6 <paulapabo@gmail.com>
2018-10-25 19:20:14 +0200

    add colors constant

---
+++ jubilapp-mobile/src/constants/colors.js
@@ -0,0 +1,15 @@
+export const HEADER_COLOR={
+    color: '#D81B60'
+}
+
+export const BACKGROUND_COLOR={
+    color: '#FFF'
+}
+
+export const BUTTON_COLOR={
+    color: '#22563a'
+}
+
+export const TEXT_COLOR={
+    color: '#848484'
+}
