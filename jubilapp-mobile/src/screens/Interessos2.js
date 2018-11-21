db32d6fdfa2619f0b8e929d506d665dc9fb44c53
Biel Simon Rojas <bielsimon2@gmail.com>
2018-11-21 19:58:38 +0100

    Arreglat checkbox interessos

--- jubilapp-mobile/src/screens/Interessos2.js
+++ jubilapp-mobile/src/screens/Interessos2.js
@@ -12,49 +12,55 @@ import {changeCreateActivityFormProperty} from "../actions";
 import connect from "react-redux/es/connect/connect";
 
 export default class Interessos2 extends React.Component {
-    interessos_info = [
+    state = {
+      interessos_info: [
         {
-            id:0,
-            estat:false,
-            icon: require('../images/artPES.jpg'),
-            nom:'Arte',
+          id:0,
+          estat:false,
+          icon: require('../images/artPES.jpg'),
+          nom:'Arte',
         },
         {
-            id:1,
-            estat:false,
-            icon: require('../images/culturaPES.png'),
-            nom:'Cultura',
+          id:1,
+          estat:false,
+          icon: require('../images/culturaPES.png'),
+          nom:'Cultura',
         },
         {
-            id:2,
-            estat:false,
-            icon: require('../images/esportPES.jpg'),
-            nom:'Deporte',
+          id:2,
+          estat:false,
+          icon: require('../images/esportPES.jpg'),
+          nom:'Deporte',
         },
         {
-            id:3,
-            estat:false,
-            icon: require('../images/excursionsPES.jpg'),
-            nom:'Excursiones',
+          id:3,
+          estat:false,
+          icon: require('../images/excursionsPES.jpg'),
+          nom:'Excursiones',
         },
         {
-            id:4,
-            estat:false,
-            icon: require('../images/ocioPES.jpg'),
-            nom:'Ocio',
+          id:4,
+          estat:false,
+          icon: require('../images/ocioPES.jpg'),
+          nom:'Ocio',
         },
         {
-            id:5,
-            estat:false,
-            icon: require('../images/tallersPES.jpg'),
-            nom:'Talleres',
+          id:5,
+          estat:false,
+          icon: require('../images/tallersPES.jpg'),
+          nom:'Talleres',
         }
 
-    ];
+      ]
+    }
+
     estats = [true,false,false,false,false,false];
     _onPressButton(interes) {
         //alert(interes.id);
-        this.estats[interes.id] = !this.estats[interes.id];
+      const new_interessos_info = this.state.interessos_info
+      new_interessos_info[interes.id] = !new_interessos_info[interes.id];
+      this.setState({interessos_info: new_interessos_info})
+
         alert(this.estats[interes.id]);
     }
     dibuixarInteressos(num)
